29f8a0411e3e610917f8c3f0f31ef5722101df69
Pamela Anderson <cyberryder2010@gmail.com>
2020-04-17 16:51:55 -0500

    inex.js reducers installed pages files added

---
+++ redux-feedback-loop-master/src/components/Pages/PageFeeling.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class PageFeeling extends Component {
+  render() {
+    return (
+      <div>
+        <h1>How are you feeling today?></h1>
+      </div>
+    );
+  }
+}
+export default connect()(PageFeeling);
--- redux-feedback-loop-master/src/index.js
+++ redux-feedback-loop-master/src/index.js
@@ -1,8 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App/App";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+
+const feelingReducer = (state = "", action) => {
+  if (action.type === "SET_FEELING") {
+    return action.payload;
+  }
+
+  return state;
+};
+
+const understandingReducer = (state = "", action) => {
+  if (action.type === "SET_UNDERSTANDING") {
+    return action.payload;
+  }
+
+  return state;
+};
+
+const supportReducer = (state = "", action) => {
+  if (action.type === "SET_SUPPORT") {
+    return action.payload;
+  }
+
+  return state;
+};
+
+const commentsReducer = (state = "", action) => {
+  if (action.type === "SET_COMMENTS") {
+    return action.payload;
+  }
+
+  return state;
+};
+
+const storeInstance = createStore(
+  combineReducers({
+    feelingReducer,
+    understandingReducer,
+    supportReducer,
+    commentsReducer,
+  }),
+  applyMiddleware(logger)
+);
+
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
